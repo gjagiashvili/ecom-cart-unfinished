7b00ae9346aab7939b84182823900d02d4d53a13
gjagiashvili <147982243+gjagiashvili@users.noreply.github.com>
2024-01-25 20:08:01 +0400

    Update Cart.jsx

--- ecommerce-site/src/components/Cart.jsx
+++ ecommerce-site/src/components/Cart.jsx
@@ -3,14 +3,18 @@ import styles from "../components/Cart.module.css";
 import cartIcon from "../images/icon-cart.svg";
 import deleteIcon from "../images/icon-delete.svg";
 
+// კომპონენტი სახელად Cart
 const Cart = () => {
-  const [cartItems, setCartItems] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [quantity, setQuantity] = useState(1);
+  const [cartItems, setCartItems] = useState([]); // წარმოადგენს კალათაში არსებულ ნივთებს
+  const [showModal, setShowModal] = useState(false); // აკონტროლებს მოდალის ხილვადობას
+  const [quantity, setQuantity] = useState(1); // წარმოადგენს დასამატებელი ნივთების რაოდენობას
 
+  // კალათაში პროდუქტის დამატების ფუნქცია
   const addToCart = (product) => {
+    // შემოწმება, არის თუ არა პროდუქტი უკვე კალათაში
     const existingItem = cartItems.find((item) => item.name === product.name);
 
+    // თუ არსებობს, ხდება არსებული ნივთის რაოდენობის განახლება
     if (existingItem) {
       const updatedCart = cartItems.map((item) =>
         item.name === product.name
@@ -18,10 +22,11 @@ const Cart = () => {
           : item
       );
       setCartItems(updatedCart);
-    } else {
+    } // თუ არა, დავამატოთ ახალი ნივთი კალათაში
+    else {
       const updatedCart = [
         ...cartItems,
-        { ...product, amount: quantity > 0 ? quantity : 1 },
+        { ...product, amount: quantity > 0 ? quantity : 1 }, // ეს ნაწილი ქმნის ახალ ობიექტს. Spread ოპერატორი ({ ...product }) გამოიყენება product ობიექტიდან ყველა property-ების ახალ ობიექტში "დასაკოპირებლად" და დამატებით ამ ობიექტს ემატება ახალი property, რომელიც პროდუქტის რაოდენობაა კალათაში.
       ];
       setCartItems(updatedCart);
     }
@@ -31,12 +36,15 @@ const Cart = () => {
   };
 
   const deleteFromCart = (productName) => {
+    // განვაახლოთ პროდუქტის რაოდენობა
     const updatedCart = cartItems.map((item) =>
       item.name === productName ? { ...item, amount: item.amount - 1 } : item
     );
 
+    // ამოვიღოთ პროდუქტები 0-ზე ნაკლები ან ტოლი რაოდენობით
     const filteredCart = updatedCart.filter((item) => item.amount > 0);
 
+    // მოდალის დამალვა თუ კალათა ცარიელია
     if (filteredCart.length === 0) {
       setShowModal(false);
     }
@@ -44,22 +52,30 @@ const Cart = () => {
     setCartItems(filteredCart);
   };
 
+  // მოდალის დახურვის ფუნქცია
   const closeModal = () => {
     setShowModal(false);
   };
 
+  // კალათის ჯამის გამოთვლის ფუნქცია
   const calculateSum = () => {
     return cartItems.reduce((sum, item) => sum + item.price * item.amount, 0);
   };
 
+  // ფუნქცია პროდუქტის რაოდენობის ცვლილებებისთვის
   const handleQuantityChange = (amount) => {
     if (quantity + amount >= 1) {
       setQuantity(quantity + amount);
     }
   };
 
+  // კალათის render ფუნქცია
   const renderCartIcon = () => {
     const totalItems = cartItems.reduce((sum, item) => sum + item.amount, 0);
+    // cartItems არის მასივი, რომელიც შეიცავს კალათაში არსებულ ნივთებს.
+    // .reduce: reduce ფუნქცია გამოიყენება მასივის ერთ მნიშვნელობად გადაქცევისთვის. იგი იღებს callback ფუნქციას თავის არგუმენტად და საწყის მნიშვნელობას.
+    // 0 არის ჯამის საწყისი მნიშვნელობა.
+
     return (
       <div
         className={styles.cartIconContainer}
